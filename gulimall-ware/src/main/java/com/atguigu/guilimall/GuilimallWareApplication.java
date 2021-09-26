78f701037b5624c6af385c625318889f44472904
masamune <qjxxzjl@gmail.com>
2021-09-26 21:42:30 +0800

    第2次上传

---
+++ gulimall-ware/src/main/java/com/atguigu/guilimall/GuilimallWareApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.guilimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuilimallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuilimallWareApplication.class, args);
+    }
+
+}
